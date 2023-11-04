19b1af3de0fc3db6d02ced4af1ab5142fc4b1b3b
Divyanka Chaudhari <60309618+divyankachaudhari@users.noreply.github.com>
2023-11-04 15:57:57 +0530

    Added $PATH variable

--- src_new/configure_raft.sh
+++ src_new/configure_raft.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cd /home/abhinav/redisraft
+# Define a variable for the path
+REDISRAFT_PATH="/Users/divyanka/redisraft"
+
+cd $REDISRAFT_PATH
 if test -f redisraft.so; then
     rm redisraft.so
 fi
@@ -12,17 +15,25 @@ cd build
 cmake ..
 make
 
+# Assuming the `redis-cli` and `redis-server` are in the user's PATH
+# Shut down the default Redis server
 redis-cli SHUTDOWN
+# Attempt to shut down a server with authentication
 redis-cli -a pass SHUTDOWN
 
+# Loop through all arguments
 for arg in "$@";
 do
+    # Shut down the Redis server listening on the port provided in the argument
     redis-cli -p $arg SHUTDOWN
-    redis-server --port $arg --daemonize yes --dbfilename raft$arg.rdb --loadmodule /home/abhinav/redisraft/redisraft.so --raft.log-filename raftlog$arg.db --raft.addr localhost:$arg
+    # Start the Redis server with the specified configurations
+    redis-server --port $arg --daemonize yes --dbfilename raft$arg.rdb --loadmodule $REDISRAFT_PATH/redisraft.so --raft.log-filename raftlog$arg.db --raft.addr localhost:$arg
 done
 
-redis-cli -p $1 raft.cluster init;
+# Initialize the raft cluster on the first port
+redis-cli -p $1 raft.cluster init
+# Join the other instances to the raft cluster
 for arg in "${@:2}";
 do
     redis-cli -p $arg RAFT.CLUSTER JOIN localhost:$1
-done;
+done
